5df294d997d6ab6c2c090fce7d79f3937cc0d819
Rodrigo Soto Cid <rodrigo.soto.web82@gmail.com>
2023-09-12 18:48:57 +0200

    Fin Sección 7

---
+++ src/pokemon/dto/create-pokemon.dto.ts
@@ -0,0 +1,13 @@
+import { IsInt, IsPositive, IsString, Min, MinLength } from "class-validator";
+
+export class CreatePokemonDto {
+
+    @IsString()
+    @MinLength(1)
+    name: string;
+
+    @IsInt()
+    @Min(1)
+    @IsPositive()
+    no: number;
+}
